8ac8ee377622df41bd918d8ce1a199566326139a
yocki24 <yockifebrianto@gmail.com>
2021-12-09 06:05:19 +0700

    update cardview

--- servis_mobil/app/src/main/java/com/android/servis_kendaraan/MenuUtama.java
+++ servis_mobil/app/src/main/java/com/android/servis_kendaraan/MenuUtama.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,45 +27,60 @@ public class MenuUtama extends AppCompatActivity {
         db = new DB(this);
 
         spLogin = getSharedPreferences(StaticVars.SP_LOGIN, MODE_PRIVATE);
-        btnInput = findViewById(R.id.btnInput);
-        btnInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SoundBtn.soundBtn(MenuUtama.this);
-                startActivity(new Intent(MenuUtama.this, InputKendaraan.class));
-            }
-        });
-        btnUpdate = findViewById(R.id.btnUpdate);
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SoundBtn.soundBtn(MenuUtama.this);
-                startActivity(new Intent(MenuUtama.this, UpdateKendaraan.class));
-            }
-        });
-        btnStatus = findViewById(R.id.btnStatus);
-        btnStatus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SoundBtn.soundBtn(MenuUtama.this);
-                startActivity(new Intent(MenuUtama.this, ListKendaraan.class));
-            }
-        });
-        btnLogout = findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SoundBtn.soundBtn(MenuUtama.this);
-                showDialog();
-            }
-        });
+//        btnInput = findViewById(R.id.btnInput);
+//        btnInput.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SoundBtn.soundBtn(MenuUtama.this);
+//                startActivity(new Intent(MenuUtama.this, InputKendaraan.class));
+//            }
+//        });
+//        btnUpdate = findViewById(R.id.btnUpdate);
+//        btnUpdate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SoundBtn.soundBtn(MenuUtama.this);
+//                startActivity(new Intent(MenuUtama.this, UpdateKendaraan.class));
+//            }
+//        });
+//        btnStatus = findViewById(R.id.btnStatus);
+//        btnStatus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SoundBtn.soundBtn(MenuUtama.this);
+//                startActivity(new Intent(MenuUtama.this, ListKendaraan.class));
+//            }
+//        });
+//        btnLogout = findViewById(R.id.btnLogout);
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SoundBtn.soundBtn(MenuUtama.this);
+//                showDialog();
+//            }
+//        });
     }
-    @Override
-    public void onBackPressed() {
-        SoundBtn.soundBtn(MenuUtama.this);
-        finish();
+
+    public void btnInput(View view)
+    {
+        Intent intent =new Intent(MenuUtama.this, InputKendaraan.class);
+        startActivity(intent);
     }
-    private void showDialog(){
+
+    public void btnStatus(View view)
+    {
+        Intent intent =new Intent(MenuUtama.this, ListKendaraan.class);
+        startActivity(intent);
+    }
+
+    public void btnUpdate(View view)
+    {
+        Intent intent =new Intent(MenuUtama.this, UpdateKendaraan.class);
+        startActivity(intent);
+    }
+
+    public void btnLogout(View view)
+    {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
                 this);
         alertDialogBuilder.setTitle("Apa benar anda ingin Logout ?");
@@ -91,4 +107,37 @@ public class MenuUtama extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
+
+    @Override
+    public void onBackPressed() {
+        SoundBtn.soundBtn(MenuUtama.this);
+        finish();
+    }
+//    private void showDialog(){
+//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+//                this);
+//        alertDialogBuilder.setTitle("Apa benar anda ingin Logout ?");
+//        alertDialogBuilder
+//                .setMessage("Klik Ya untuk logout!")
+//                .setCancelable(false)
+//                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog,int id) {
+//                        SoundBtn.soundBtn(MenuUtama.this);
+//                        SharedPreferences.Editor loginEditor = spLogin.edit();
+//                        loginEditor.clear();
+//                        loginEditor.apply();
+//                        finish();
+//                        startActivity(new Intent(MenuUtama.this,Login.class));
+//                    }
+//                })
+//                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        SoundBtn.soundBtn(MenuUtama.this);
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//        AlertDialog alertDialog = alertDialogBuilder.create();
+//        alertDialog.show();
+//    }
 }
